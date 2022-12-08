e603debb525b2b374f6a707601cb11775a712a73
N44MO <120064414+N44MO@users.noreply.github.com>
2022-12-08 09:53:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ API_HASH = getenv("API_HASH", "") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5810901257").split()))
 OWNER_ID = int(getenv("OWNER_ID", "5810901257")) #ur id
-MONGO_URL = getenv("MONGO_URL") # an database
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Namo:Namo@cluster0.5xig19t.mongodb.net/?retryWrites=true&w=majority") # an database
+BOT_TOKEN = getenv("BOT_TOKEN", "‌5902872993:AAFKSUq4y1HHOHaceAiKG9hGO8j0kAuCY_k")
 ALIVE_PIC = getenv("ALIVE_PIC")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
+ALIVE_TEXT = getenv("ALIVE_TEXT") # I'd if uh want
+PM_LOGGER = getenv("PM_LOGGER") #id if want to enable tagalert
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "AgBiMZkAYdUQoYL07SfQ_QaXDnlouO1lVOqBF8KcTB55r-PPSh8nl-2z5hOdS9OObne6hq3_mQ0NhHS5SGXnt-V3j0DjKTTIn9MWJys3OJW7YURY27olvsU52rlex2CrDK5P2R68qiwdomes742Wq8mMORR1YF3yhGgR1QE8oaLN0zkqN9x7oP_D0kWNlZ7HA-ra8_FgkpSKOSWE76yczA-mGZ97Ge9ZTHQdeU6J8xE-dhu5F55Fpt04D0W2knowAFPli9s8yWNg6B_sjtoWn7AlwhylwfueoN2PkXx01CrrB0YD9Odw7PtLhjud7eIauA4JfYgUAsBbm2anR3AM6y13SFWU9QAAAAFaW1EJAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
